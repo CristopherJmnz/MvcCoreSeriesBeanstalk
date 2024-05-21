c4314e0b40a9af957c42979d5947b328a4249240
CristopherJmnz <crisjimenez19@gmail.com>
2024-05-21 12:29:43 +0200

    Add project files.

---
+++ MvcCoreSeriesBeanstalk/Controllers/SeriesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSeriesBeanstalk.Models;
+using MvcCoreSeriesBeanstalk.Repositories;
+
+namespace MvcCoreSeriesBeanstalk.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private SeriesRepository repo;
+        public SeriesController(SeriesRepository repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Serie> series = await this.repo.GetSeriesAsync();
+            return View(series);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Serie serie)
+        {
+            await this.repo.InsertSerie(serie.Nombre, serie.Imagen, serie.Year);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreSeriesBeanstalk/Repositories/SeriesRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreSeriesBeanstalk.Data;
+using MvcCoreSeriesBeanstalk.Models;
+
+namespace MvcCoreSeriesBeanstalk.Repositories
+{
+    public class SeriesRepository
+    {
+        private SeriesContext context;
+        public SeriesRepository(SeriesContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Serie>> GetSeriesAsync()
+        {
+            return await this.context.Series.ToListAsync();
+        }
+
+        private async Task<int> GetMaxIdSerieAsync()
+        {
+            return await this.context.Series
+                .MaxAsync(x => x.IdSerie) + 1;
+        }
+
+        public async Task InsertSerie(string nombre,string imagen,int year)
+        {
+            Serie serie = new Serie()
+            {
+                IdSerie = await this.GetMaxIdSerieAsync(),
+                Imagen = imagen,
+                Nombre = nombre,
+                Year = year
+            };
+            await this.context.Series.AddAsync(serie);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
